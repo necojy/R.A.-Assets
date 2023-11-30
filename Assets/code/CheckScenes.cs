8e4af992456a961dca6472abbfe3f140bf1ecad8
necOjy <jim2323098@gmail.com>
2023-12-01 01:58:10 +0800

    修正背景音樂撥放問題，降低小boss血量

--- Assets/code/CheckScenes.cs
+++ Assets/code/CheckScenes.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class CheckScenes : MonoBehaviour
 {
     //檢查當前場景
-    public void Start() 
-    { 
+    public void Start()
+    {
         Scene currentScene = SceneManager.GetActiveScene();
-        if(currentScene.name == "Factory02")
+
+        if (AudioManager.Instance == null)
+        {
+            // 初始化 AudioManager 或者在這裡進行其他必要的處理
+            Debug.LogError("AudioManager is not initialized!");
+            return;
+        }
+
+        if (currentScene.name == "Factory02")
+        {
+            AudioManager.Instance.PlayBackground("Factory Map Music");
+        }
+    }
+    public void Awake()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if (AudioManager.Instance == null)
+        {
+            // 初始化 AudioManager 或者在這裡進行其他必要的處理
+            Debug.LogError("AudioManager is not initialized!");
+            return;
+        }
+
+        if (currentScene.name == "Factory02")
         {
             AudioManager.Instance.PlayBackground("Factory Map Music");
         }
