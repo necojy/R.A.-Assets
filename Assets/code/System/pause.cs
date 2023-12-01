ac7d029a1876431b2de608f77897c7b0e1de4166
cloud <yc359032@gmail.com>
2023-12-01 20:37:45 +0800

    update iu

--- Assets/code/System/pause.cs
+++ Assets/code/System/pause.cs
@@ -1,30 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pause : MonoBehaviour
 {
     PlayerMove playerMove;
     PlayerAttack playerAttack;
-    public GameObject play_button;
+    Transform parentTransform;
     private void Start()
     {
         playerMove = GameObject.Find("Player").GetComponent<PlayerMove>();
         playerAttack = GameObject.Find("Player").GetComponent<PlayerAttack>();
-        play_button.SetActive(false);
+        parentTransform = transform;
+
+        Hide();
     }
     public void Show()
     {
         Time.timeScale = 0;
         playerMove.isInputEnabled = false;
         playerAttack.isAttackable = false;
-        play_button.SetActive(true);
+        // 遍历所有子物件
+        foreach (Transform child in parentTransform)
+        {
+            // 将每个子物件的SetActive属性设置为false
+            child.gameObject.SetActive(true);
+        }
     }
     public void Hide()
     {
         Time.timeScale = 1;
         playerMove.isInputEnabled = true;
         playerAttack.isAttackable = true;
-        play_button.SetActive(false);
+        foreach (Transform child in parentTransform)
+        {
+            // 将每个子物件的SetActive属性设置为false
+            child.gameObject.SetActive(false);
+        }
+    }
+    public void home()
+    {
+        Hide();
+        SceneManager.LoadScene("Menu");
+    }
+    public void exit()
+    {
+        Application.Quit();
     }
 }
--- Assets/code/物件/Treasure.cs
+++ Assets/code/物件/Treasure.cs
@@ -4,54 +4,18 @@ using UnityEngine;
 
 public class Treasure : MonoBehaviour
 {
-    public GameObject coin;
-
-    public float coinSpeed = 5f;//向外拋出金幣的速度
-    private bool canOpen;//確認玩家是否能開寶箱
-    private bool BoxStatus;//確認寶箱是否被開啟
-    private Animator anim;
-
-
-    void Start()
-    {
-        BoxStatus = false;
-    }
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.I))
-        {
-            if(canOpen && !BoxStatus)
-            {
-                BoxStatus = true;
-                SpawnCoins();
-                Destroy(gameObject);
-            }
-        }
-    }
-
+    bool SetHp = true;
     void OnTriggerEnter2D(Collider2D Other)
     {
-        if(Other.gameObject.CompareTag("Player"))
+        if (Other.gameObject.CompareTag("Player"))
         {
-            canOpen = true;
+            gameObject.SetActive(false);
+            Debug.Log("Hello, this is a debug message!");
+            if (SetHp)
+            {
+                PlayerPrefs.SetInt("Hp", PlayerPrefs.GetInt("Hp") + 1);
+                SetHp = false;
+            }
         }
-    }
-
-    void OnTriggerExit2D(Collider2D Other)
-    {
-        if(Other.gameObject.CompareTag("Player"))
-        {
-            canOpen = false;
-        }
-    }
-
-    void SpawnCoins()
-    {
-        AudioManager.Instance.PlayItem("Treature");
-        Rigidbody2D coinInstance = Instantiate(coin, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
-        // 設置金幣的旋轉角度
-        Rigidbody2D coinRigidbody = coinInstance.GetComponent<Rigidbody2D>();
-        coinRigidbody.AddForce(Vector2.up * coinSpeed, ForceMode2D.Impulse);
     }
 }
